2e81ed4392078f0fe7497bb62e7151c84d54f707
Xiangyin Ma <x1.ma@samsung.com>
2015-02-03 13:38:28 +0000

    BloomView & GaussianBlurView demo (for verification purpose only )
    Change-Id: I7279b5cb4c5b157b618fb184fac50e710463cf51

--- examples/magnifier/magnifier-example.cpp
+++ examples/magnifier/magnifier-example.cpp
@@ -236,7 +236,7 @@ public:
     mMagnifier.SetSourceActor( mView.GetBackgroundLayer() );
     mMagnifier.SetSize( MAGNIFIER_SIZE * mStageSize.width );  // Size of magnifier is in relation to stage width
     mMagnifier.SetMagnificationFactor( MAGNIFICATION_FACTOR );
-    mMagnifier.SetFrameVisibility(false);
+    mMagnifier.SetFrameVisibility(true);
     mMagnifier.SetScale(Vector3::ZERO);
     overlay.Add( mMagnifier );
 
