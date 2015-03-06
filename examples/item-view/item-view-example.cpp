tizenorg/platform.core.uifw.dali-demo

Property refactor in dali-toolkit: Demo changes for compiling
Change-Id: I18d69dba01824ffaa1cd3a4c6b9ead3a38981bc7

examples/item-view/item-view-example.cpp
@@ -971,11 +971,11 @@ private:
     mMenu.Add( tableView );
 
     Slider slider = Slider::New();
-    slider.SetProperty( Slider::LOWER_BOUND_PROPERTY, 0.0f );
-    slider.SetProperty( Slider::UPPER_BOUND_PROPERTY, 3.0f );
-    slider.SetProperty( Slider::VALUE_PROPERTY, mDurationSeconds );
-    slider.SetProperty( Slider::VALUE_PRECISION_PROPERTY, 2 );
-    slider.SetProperty( Slider::SHOW_POPUP_PROPERTY, true );
+    slider.SetProperty( Slider::Property::LowerBound, 0.0f );
+    slider.SetProperty( Slider::Property::UpperBound, 3.0f );
+    slider.SetProperty( Slider::Property::Value, mDurationSeconds );
+    slider.SetProperty( Slider::Property::ValuePrecision, 2 );
+    slider.SetProperty( Slider::Property::ShowPopup, true );
     slider.ValueChangedSignal().Connect( this, &ItemViewExample::SliderValueChange );
     tableView.AddChild( slider, TableView::CellPosition( 0, 0 ) );
     tableView.SetRelativeHeight( 0, 0.5f );

dalihub/dali-demo

examples/item-view/item-view-example.cpp
@@ -971,11 +971,11 @@ private:
     mMenu.Add( tableView );
 
     Slider slider = Slider::New();
-    slider.SetProperty( Slider::LOWER_BOUND_PROPERTY, 0.0f );
-    slider.SetProperty( Slider::UPPER_BOUND_PROPERTY, 3.0f );
-    slider.SetProperty( Slider::VALUE_PROPERTY, mDurationSeconds );
-    slider.SetProperty( Slider::VALUE_PRECISION_PROPERTY, 2 );
-    slider.SetProperty( Slider::SHOW_POPUP_PROPERTY, true );
+    slider.SetProperty( Slider::Property::LOWER_BOUND, 0.0f );
+    slider.SetProperty( Slider::Property::UPPER_BOUND, 3.0f );
+    slider.SetProperty( Slider::Property::VALUE, mDurationSeconds );
+    slider.SetProperty( Slider::Property::VALUE_PRECISION, 2 );
+    slider.SetProperty( Slider::Property::SHOW_POPUP, true );
     slider.ValueChangedSignal().Connect( this, &ItemViewExample::SliderValueChange );
     tableView.AddChild( slider, TableView::CellPosition( 0, 0 ) );
     tableView.SetRelativeHeight( 0, 0.5f );