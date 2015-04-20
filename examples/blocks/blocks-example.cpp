tizenorg/platform.core.uifw.dali-demo

Cleanup for removal of ImageAttributes from public API
Change-Id: I5bafc645c223b173abdd20db00094c501c846ee4
Signed-off-by: Andrew Cox <andrew.cox@partner.samsung.com>

examples/blocks/blocks-example.cpp
@@ -587,10 +587,7 @@ private:
     Vector2 stageSize(Stage::GetCurrent().GetSize());
     const Vector2 brickSize(BRICK_SIZE * Vector2(stageSize.x, stageSize.x));
 
-    ImageAttributes attr;
-    attr.SetSize( 128, 64 );
-    attr.SetScalingMode( ImageAttributes::ScaleToFill );
-    Image img = ResourceImage::New(BRICK_IMAGE_PATH[type], attr);
+    Image img = ResourceImage::New( BRICK_IMAGE_PATH[type], Dali::ImageDimensions( 128, 64 ), Dali::ScalingMode::ScaleToFill, Dali::SamplingMode::BoxThenLinear );
     ImageActor brick = ImageActor::New(img);
     brick.SetParentOrigin(ParentOrigin::TOP_LEFT);
     brick.SetAnchorPoint(AnchorPoint::CENTER);

examples/cube-transition-effect/cube-transition-effect-example.cpp
@@ -89,19 +89,15 @@ const int VIEWINGTIME = 2000; // 2 seconds
 /**
  * @brief Load an image, scaled-down to no more than the stage dimensions.
  *
- * Uses image scaling mode ImageAttributes::ScaleToFill to resize the image at
+ * Uses image scaling mode ScaleToFill to resize the image at
  * load time to cover the entire stage with pixels with no borders,
- * and filter mode ImageAttributes::BoxThenLinear to sample the image with
+ * and filter mode BoxThenLinear to sample the image with
  * maximum quality.
  */
 ResourceImage LoadStageFillingImage( const char * const imagePath )
 {
   Size stageSize = Stage::GetCurrent().GetSize();
-  ImageAttributes attributes;
-  attributes.SetSize( stageSize.x, stageSize.y );
-  attributes.SetFilterMode( ImageAttributes::BoxThenLinear );
-  attributes.SetScalingMode( ImageAttributes::ScaleToFill );
-  return ResourceImage::New( imagePath, attributes );
+  return ResourceImage::New( imagePath, ImageDimensions( stageSize.x, stageSize.y ), Dali::ScalingMode::ScaleToFill, Dali::SamplingMode::BoxThenLinear );
 }
 
 } // namespace

examples/new-window/new-window-example.cpp
@@ -220,8 +220,9 @@ FrameBufferImage NewWindowController::CreateMirrorImage(const char* imageName)
 ImageActor NewWindowController::CreateBlurredMirrorImage(const char* imageName)
 {
   FrameBufferImage fbo;
-  Image image = ResourceImage::New( imageName );
-  Vector2 FBOSize = ResourceImage::GetImageSize(imageName);
+  Image image = ResourceImage::New(imageName);
+  Vector2Uint16 intFboSize = ResourceImage::GetImageSize(imageName);
+  Vector2 FBOSize = Vector2( intFboSize.GetWidth(), intFboSize.GetHeight() );
   fbo = FrameBufferImage::New( FBOSize.width, FBOSize.height, Pixel::RGBA8888);
   GaussianBlurView gbv = GaussianBlurView::New(5, 2.0f, Pixel::RGBA8888, 0.5f, 0.5f, true);
   gbv.SetBackgroundColor(Color::TRANSPARENT);
@@ -239,7 +240,8 @@ ImageActor NewWindowController::CreateBlurredMirrorImage(const char* imageName)
 FrameBufferImage NewWindowController::CreateFrameBufferForImage(const char* imageName, Image image, ShaderEffect shaderEffect)
 {
   Stage stage = Stage::GetCurrent();
-  Vector2 FBOSize = ResourceImage::GetImageSize(imageName);
+  Vector2Uint16 intFboSize = ResourceImage::GetImageSize(imageName);
+  Vector2 FBOSize = Vector2(intFboSize.GetWidth(), intFboSize.GetHeight());
 
   FrameBufferImage framebuffer = FrameBufferImage::New(FBOSize.x, FBOSize.y );
 

examples/radial-menu/radial-menu-example.cpp
@@ -147,7 +147,9 @@ void RadialMenuExample::OnInit(Application& app)
                       Toolkit::Alignment::HorizontalRight,
                       DemoHelper::DEFAULT_PLAY_PADDING );
 
-  Vector2 imgSize = ResourceImage::GetImageSize(TEST_OUTER_RING_FILENAME);
+
+  const Vector2Uint16 intImgSize = ResourceImage::GetImageSize(TEST_OUTER_RING_FILENAME);
+  Vector2 imgSize = Vector2( intImgSize.GetWidth(), intImgSize.GetHeight() );
   Vector2 stageSize = stage.GetSize();
   float minStageDimension = std::min(stageSize.width, stageSize.height);
 
@@ -244,8 +246,8 @@ RadialSweepView RadialMenuExample::CreateSweepView( std::string imageName,
   mImageActor.SetAnchorPoint(AnchorPoint::CENTER);
 
   // Create the stencil
-  Vector2 imageSize = ResourceImage::GetImageSize(imageName);
-  float diameter = std::max(imageSize.width, imageSize.height);
+  const Vector2Uint16 imageSize = ResourceImage::GetImageSize(imageName);
+  float diameter = std::max(imageSize.GetWidth(), imageSize.GetHeight());
   RadialSweepView radialSweepView = RadialSweepView::New();
   radialSweepView.SetDiameter( diameter );
   radialSweepView.SetInitialAngle( initialAngle );

shared/view.h
@@ -150,11 +150,7 @@ Dali::Layer CreateView( Dali::Application& application,
   // Set background image, loading it at screen resolution:
   if ( !backgroundImagePath.empty() )
   {
-    Dali::ImageAttributes backgroundAttributes;
-    backgroundAttributes.SetSize( stage.GetSize() );
-    backgroundAttributes.SetFilterMode( Dali::ImageAttributes::BoxThenLinear );
-    backgroundAttributes.SetScalingMode( Dali::ImageAttributes::ScaleToFill );
-    Dali::Image backgroundImage = Dali::ResourceImage::New( backgroundImagePath, backgroundAttributes );
+    Dali::Image backgroundImage = Dali::ResourceImage::New( backgroundImagePath, Dali::ImageDimensions( stage.GetSize().x, stage.GetSize().y ), Dali::ScalingMode::ScaleToFill, Dali::SamplingMode::BoxThenLinear );
     Dali::ImageActor backgroundImageActor = Dali::ImageActor::New( backgroundImage );
     view.SetBackground( backgroundImageActor );
   }

dalihub/dali-demo

examples/blocks/blocks-example.cpp
@@ -524,10 +524,7 @@ private:
     Vector2 stageSize(Stage::GetCurrent().GetSize());
     const Vector2 brickSize(BRICK_SIZE * Vector2(stageSize.x, stageSize.x));
 
-    ImageAttributes attr;
-    attr.SetSize( 128, 64 );
-    attr.SetScalingMode( ImageAttributes::ScaleToFill );
-    Image img = ResourceImage::New(BRICK_IMAGE_PATH[type], attr);
+    Image img = ResourceImage::New( BRICK_IMAGE_PATH[type], Dali::ImageDimensions( 128, 64 ), Dali::FittingMode::SCALE_TO_FILL, Dali::SamplingMode::BOX_THEN_LINEAR );
     ImageActor brick = ImageActor::New(img);
     brick.SetParentOrigin(ParentOrigin::TOP_LEFT);
     brick.SetAnchorPoint(AnchorPoint::CENTER);

examples/cube-transition-effect/cube-transition-effect-example.cpp
@@ -91,19 +91,15 @@ const int VIEWINGTIME = 2000; // 2 seconds
 /**
  * @brief Load an image, scaled-down to no more than the stage dimensions.
  *
- * Uses image scaling mode ImageAttributes::ScaleToFill to resize the image at
+ * Uses image scaling mode SCALE_TO_FILL to resize the image at
  * load time to cover the entire stage with pixels with no borders,
- * and filter mode ImageAttributes::BoxThenLinear to sample the image with
+ * and filter mode BOX_THEN_LINEAR to sample the image with
  * maximum quality.
  */
 ResourceImage LoadStageFillingImage( const char * const imagePath )
 {
   Size stageSize = Stage::GetCurrent().GetSize();
-  ImageAttributes attributes;
-  attributes.SetSize( stageSize.x, stageSize.y );
-  attributes.SetFilterMode( ImageAttributes::BoxThenLinear );
-  attributes.SetScalingMode( ImageAttributes::ScaleToFill );
-  return ResourceImage::New( imagePath, attributes );
+  return ResourceImage::New( imagePath, ImageDimensions( stageSize.x, stageSize.y ), Dali::FittingMode::SCALE_TO_FILL, Dali::SamplingMode::BOX_THEN_LINEAR );
 }
 
 } // namespace

examples/new-window/new-window-example.cpp
@@ -222,8 +222,9 @@ FrameBufferImage NewWindowController::CreateMirrorImage(const char* imageName)
 ImageActor NewWindowController::CreateBlurredMirrorImage(const char* imageName)
 {
   FrameBufferImage fbo;
-  Image image = ResourceImage::New( imageName );
-  Vector2 FBOSize = ResourceImage::GetImageSize(imageName);
+  Image image = ResourceImage::New(imageName);
+  Uint16Pair intFboSize = ResourceImage::GetImageSize(imageName);
+  Vector2 FBOSize = Vector2( intFboSize.GetWidth(), intFboSize.GetHeight() );
   fbo = FrameBufferImage::New( FBOSize.width, FBOSize.height, Pixel::RGBA8888);
   GaussianBlurView gbv = GaussianBlurView::New(5, 2.0f, Pixel::RGBA8888, 0.5f, 0.5f, true);
   gbv.SetBackgroundColor(Color::TRANSPARENT);
@@ -241,7 +242,8 @@ ImageActor NewWindowController::CreateBlurredMirrorImage(const char* imageName)
 FrameBufferImage NewWindowController::CreateFrameBufferForImage(const char* imageName, Image image, ShaderEffect shaderEffect)
 {
   Stage stage = Stage::GetCurrent();
-  Vector2 FBOSize = ResourceImage::GetImageSize(imageName);
+  Uint16Pair intFboSize = ResourceImage::GetImageSize(imageName);
+  Vector2 FBOSize = Vector2(intFboSize.GetWidth(), intFboSize.GetHeight());
 
   FrameBufferImage framebuffer = FrameBufferImage::New(FBOSize.x, FBOSize.y );
 

examples/radial-menu/radial-menu-example.cpp
@@ -149,7 +149,9 @@ void RadialMenuExample::OnInit(Application& app)
                       Toolkit::Alignment::HorizontalRight,
                       DemoHelper::DEFAULT_PLAY_PADDING );
 
-  Vector2 imgSize = ResourceImage::GetImageSize(TEST_OUTER_RING_FILENAME);
+
+  const Uint16Pair intImgSize = ResourceImage::GetImageSize(TEST_OUTER_RING_FILENAME);
+  Vector2 imgSize = Vector2( intImgSize.GetWidth(), intImgSize.GetHeight() );
   Vector2 stageSize = stage.GetSize();
   float minStageDimension = std::min(stageSize.width, stageSize.height);
 
@@ -248,8 +250,8 @@ RadialSweepView RadialMenuExample::CreateSweepView( std::string imageName,
   mImageActor.SetResizePolicy( ResizePolicy::USE_NATURAL_SIZE, Dimension::ALL_DIMENSIONS );
 
   // Create the stencil
-  Vector2 imageSize = ResourceImage::GetImageSize(imageName);
-  float diameter = std::max(imageSize.width, imageSize.height);
+  const Uint16Pair imageSize = ResourceImage::GetImageSize(imageName);
+  float diameter = std::max(imageSize.GetWidth(), imageSize.GetHeight());
   RadialSweepView radialSweepView = RadialSweepView::New();
   radialSweepView.SetDiameter( diameter );
   radialSweepView.SetInitialAngle( initialAngle );

shared/view.h
@@ -139,11 +139,7 @@ Dali::Layer CreateView( Dali::Application& application,
   // Set background image, loading it at screen resolution:
   if ( !backgroundImagePath.empty() )
   {
-    Dali::ImageAttributes backgroundAttributes;
-    backgroundAttributes.SetSize( stage.GetSize() );
-    backgroundAttributes.SetFilterMode( Dali::ImageAttributes::BoxThenLinear );
-    backgroundAttributes.SetScalingMode( Dali::ImageAttributes::ScaleToFill );
-    Dali::Image backgroundImage = Dali::ResourceImage::New( backgroundImagePath, backgroundAttributes );
+    Dali::Image backgroundImage = Dali::ResourceImage::New( backgroundImagePath, Dali::ImageDimensions( stage.GetSize().x, stage.GetSize().y ), Dali::FittingMode::SCALE_TO_FILL, Dali::SamplingMode::BOX_THEN_LINEAR );
     Dali::ImageActor backgroundImageActor = Dali::ImageActor::New( backgroundImage );
     view.SetBackground( backgroundImageActor );
   }