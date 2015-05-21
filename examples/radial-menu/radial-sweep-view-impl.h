tizenorg/platform.core.uifw.dali-demo

Update refraction-effect & radial-menu to use new mesh
Change-Id: I6eefdc77aa22886cc881df0f1ca0168251983814

examples/radial-menu/radial-sweep-view-impl.h
@@ -1,0 +1,118 @@
+#ifndef DALI_DEMO_RADIAL_SWEEP_VIEW_IMPL_H
+#define DALI_DEMO_RADIAL_SWEEP_VIEW_IMPL_H
+
+/*
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <dali-toolkit/dali-toolkit.h>
+#include "radial-sweep-view.h"
+
+
+/********************************************************************************
+ * Class to implement a layer with a radial sweep stencil mask and an actor tree
+ */
+class RadialSweepViewImpl : public Dali::Toolkit::Internal::Control
+{
+public:
+  static RadialSweepView New();
+
+  static RadialSweepView New( float duration,
+                              float diameter,
+                              Dali::Degree initialAngle,
+                              Dali::Degree finalAngle,
+                              Dali::Degree initialSector,
+                              Dali::Degree finalSector );
+
+  RadialSweepViewImpl( float duration,
+                       float diameter,
+                       Dali::Degree initialAngle,
+                       Dali::Degree finalAngle,
+                       Dali::Degree initialSector,
+                       Dali::Degree finalSector );
+
+  void SetDuration(float duration);
+  void SetEasingFunction( Dali::AlphaFunction easingFunction );
+
+  void SetDiameter(float diameter);
+  void SetInitialAngle( Dali::Degree initialAngle);
+  void SetFinalAngle( Dali::Degree finalAngle);
+  void SetInitialSector( Dali::Degree initialSector);
+  void SetFinalSector( Dali::Degree finalSector);
+  void SetInitialActorAngle( Dali::Degree initialAngle );
+  void SetFinalActorAngle( Dali::Degree finalAngle );
+
+  float GetDuration( );
+  float GetDiameter( );
+  Dali::Degree GetInitialAngle( );
+  Dali::Degree GetFinalAngle( );
+  Dali::Degree GetInitialSector( );
+  Dali::Degree GetFinalSector( );
+  Dali::Degree GetInitialActorAngle( );
+  Dali::Degree GetFinalActorAngle( );
+
+  void RotateActorsWithStencil(bool rotate);
+
+  void Add( Dali::Actor actor );
+
+  void Activate( Dali::Animation anim = Dali::Animation(), float offsetTime=0, float duration=2.0f );
+
+  void Deactivate();
+
+private:
+
+  /**
+   * Create the stencil mask
+   */
+  void CreateStencil(Dali::Degree initialSector );
+
+private:
+  Dali::Layer           mLayer;
+  Dali::Animation       mAnim;
+  float                 mDuration;
+  float                 mDiameter;
+  Dali::Degree          mInitialAngle;
+  Dali::Degree          mFinalAngle;
+  Dali::Degree          mInitialSector;
+  Dali::Degree          mFinalSector;
+  Dali::Degree          mInitialActorAngle;
+  Dali::Degree          mFinalActorAngle;
+  Dali::AlphaFunction   mEasingFunction;
+  Dali::Actor           mStencilActor;       ///< Stencil actor which generates mask
+  Dali::Property::Index mStartAngleIndex;    ///< Index of start-angle property
+  Dali::Property::Index mRotationAngleIndex; ///< Index of rotation-angle property
+  bool                  mRotateActorsWithStencil:1;
+  bool                  mRotateActors;
+};
+
+
+inline RadialSweepViewImpl& GetImpl( RadialSweepView& obj )
+{
+  DALI_ASSERT_ALWAYS(obj);
+  Dali::RefObject& handle = obj.GetImplementation();
+  return static_cast<RadialSweepViewImpl&>(handle);
+}
+
+inline const RadialSweepViewImpl& GetImpl( const RadialSweepView& obj )
+{
+  DALI_ASSERT_ALWAYS(obj);
+  const Dali::RefObject& handle = obj.GetImplementation();
+  return static_cast<const RadialSweepViewImpl&>(handle);
+}
+
+
+
+#endif // DALI_DEMO_RADIAL_SWEEP_VIEW_IMPL_H

examples/radial-menu/radial-sweep-view.cpp
@@ -1,0 +1,184 @@
+/*
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "radial-sweep-view.h"
+#include "radial-sweep-view-impl.h"
+
+using namespace Dali;
+
+RadialSweepView::RadialSweepView()
+{
+}
+
+RadialSweepView::RadialSweepView(const RadialSweepView& handle)
+: Control(handle)
+{
+}
+
+RadialSweepView& RadialSweepView::operator=(const RadialSweepView& rhs)
+{
+  if( &rhs != this )
+  {
+    Control::operator=(rhs);
+  }
+  return *this;
+}
+
+RadialSweepView::~RadialSweepView()
+{
+}
+
+RadialSweepView RadialSweepView::DownCast( BaseHandle handle )
+{
+  return Control::DownCast<RadialSweepView, RadialSweepViewImpl>(handle);
+}
+
+RadialSweepView RadialSweepView::New( )
+{
+  return RadialSweepViewImpl::New();
+}
+
+RadialSweepView RadialSweepView::New( float duration,
+                                      float diameter,
+                                      Degree initialAngle,
+                                      Degree finalAngle,
+                                      Degree initialSector,
+                                      Degree finalSector )
+{
+  return RadialSweepViewImpl::New(duration, diameter, initialAngle, finalAngle, initialSector, finalSector );
+}
+
+RadialSweepView::RadialSweepView( RadialSweepViewImpl& impl )
+: Control( impl )
+{
+}
+
+RadialSweepView::RadialSweepView( Dali::Internal::CustomActor* impl )
+: Control( impl )
+{
+  VerifyCustomActorPointer<RadialSweepViewImpl>(impl);
+}
+
+void RadialSweepView::SetDuration(float duration)
+{
+  GetImpl(*this).SetDuration(duration);
+}
+
+void RadialSweepView::SetEasingFunction( Dali::AlphaFunction easingFunction )
+{
+  GetImpl(*this).SetEasingFunction( easingFunction );
+}
+
+void RadialSweepView::SetDiameter(float diameter)
+{
+  GetImpl(*this).SetDiameter(diameter);
+}
+
+void RadialSweepView::SetInitialAngle( Dali::Degree initialAngle)
+{
+  GetImpl(*this).SetInitialAngle(initialAngle);
+}
+
+void RadialSweepView::SetFinalAngle( Dali::Degree finalAngle)
+{
+  GetImpl(*this).SetFinalAngle(finalAngle);
+}
+
+void RadialSweepView::SetInitialSector( Dali::Degree initialSector)
+{
+  GetImpl(*this).SetInitialSector(initialSector);
+}
+
+void RadialSweepView::SetFinalSector( Dali::Degree finalSector)
+{
+  GetImpl(*this).SetFinalSector(finalSector);
+}
+
+void RadialSweepView::SetInitialActorAngle( Dali::Degree initialAngle )
+{
+  GetImpl(*this).SetInitialActorAngle(initialAngle);
+}
+
+void RadialSweepView::SetFinalActorAngle( Dali::Degree finalAngle )
+{
+  GetImpl(*this).SetFinalActorAngle(finalAngle);
+}
+
+float RadialSweepView::GetDuration( )
+{
+  return GetImpl(*this).GetDuration();
+}
+
+float RadialSweepView::GetDiameter( )
+{
+  return GetImpl(*this).GetDiameter();
+}
+
+Dali::Degree RadialSweepView::GetInitialAngle( )
+{
+  return GetImpl(*this).GetInitialAngle();
+}
+
+Dali::Degree RadialSweepView::GetFinalAngle( )
+{
+  return GetImpl(*this).GetFinalAngle();
+}
+
+Dali::Degree RadialSweepView::GetInitialSector( )
+{
+  return GetImpl(*this).GetInitialSector();
+}
+
+Dali::Degree RadialSweepView::GetFinalSector( )
+{
+  return GetImpl(*this).GetFinalSector();
+}
+
+Dali::Degree RadialSweepView::GetInitialActorAngle( )
+{
+  return GetImpl(*this).GetInitialActorAngle();
+}
+
+Dali::Degree RadialSweepView::GetFinalActorAngle( )
+{
+  return GetImpl(*this).GetFinalActorAngle();
+}
+
+void RadialSweepView::RotateActorsWithStencil(bool rotate)
+{
+  GetImpl(*this).RotateActorsWithStencil(rotate);
+}
+
+void RadialSweepView::Add(Actor actor)
+{
+  GetImpl(*this).Add(actor);
+}
+
+void RadialSweepView::Activate()
+{
+  GetImpl(*this).Activate();
+}
+
+void RadialSweepView::Activate( Dali::Animation anim, float offsetTime, float duration )
+{
+  GetImpl(*this).Activate(anim, offsetTime, duration);
+}
+
+void RadialSweepView::Deactivate()
+{
+  GetImpl(*this).Deactivate();
+}

examples/radial-menu/radial-sweep-view.h
@@ -1,0 +1,150 @@
+#ifndef DALI_DEMO_RADIAL_SWEEP_VIEW_H
+#define DALI_DEMO_RADIAL_SWEEP_VIEW_H
+
+/*
+ * Copyright (c) 2014 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <dali-toolkit/dali-toolkit.h>
+
+class RadialSweepViewImpl;
+
+
+/********************************************************************************
+ * Handle to RadialSweepView implementation
+ */
+class RadialSweepView : public Dali::Toolkit::Control
+{
+public:
+  /**
+   * Create a new RadialSweepView with default parameters (2 second animation,
+   * no rotation, sweeping out a full cicle).
+   */
+  static RadialSweepView New( );
+
+  /**
+   * Create a new RadialSweepView.
+   * @param[in] duration The duration of the sweep animation
+   * @param[in] diameter The diameter of the stencil mask
+   * @param[in] initialAngle The initial angle of the anticlockwise line of the sweep sector
+   * @param[in] finalAngle The final angle of the anticlockwise line of the sweep sector
+   * @param[in] initialSector The angle of the starting sector
+   * @param[in] finalSector The angle of the sector at the end of the animation.
+   * Note, to cover the entire circle, use a value of 359.9999f, not zero or 360.
+   *
+   *            initial sector
+   *             \   |  .
+   *              \  |    .
+   * initialAngle  \ |     .   final sector
+   *                \|      _|
+   *                 .________
+   */
+  static RadialSweepView New( float duration,
+                              float diameter,
+                              Dali::Degree initialAngle,
+                              Dali::Degree finalAngle,
+                              Dali::Degree initialSector,
+                              Dali::Degree finalSector );
+
+  void SetDuration(float duration);
+
+  void SetEasingFunction( Dali::AlphaFunction easingFunction );
+
+  void SetDiameter(float diameter);
+
+  void SetInitialAngle( Dali::Degree initialAngle);
+
+  void SetFinalAngle( Dali::Degree finalAngle);
+
+  void SetInitialSector( Dali::Degree initialSector);
+
+  void SetFinalSector( Dali::Degree finalSector);
+
+  void SetInitialActorAngle( Dali::Degree initialAngle );
+
+  void SetFinalActorAngle( Dali::Degree finalAngle );
+
+  float GetDuration( );
+
+  float GetDiameter( );
+
+  Dali::Degree GetInitialAngle( );
+
+  Dali::Degree GetFinalAngle( );
+
+  Dali::Degree GetInitialSector( );
+
+  Dali::Degree GetFinalSector( );
+
+  Dali::Degree GetInitialActorAngle( );
+
+  Dali::Degree GetFinalActorAngle( );
+
+  /**
+   * @param[in] rotate True if the actors should rotate with the stencil
+   */
+  void RotateActorsWithStencil(bool rotate);
+
+  /**
+   * Add actors to the view
+   */
+  void Add(Actor actor);
+
+  /**
+   * Activate the sweep animation
+   */
+  void Activate( );
+
+  void Activate( Dali::Animation anim, float offsetTime, float duration );
+
+  /**
+   * Deactivate the sweep animation
+   */
+  void Deactivate();
+
+  /**
+   * Default constructor. Create an uninitialized handle.
+   */
+  RadialSweepView();
+
+  /**
+   * Copy constructor
+   */
+  RadialSweepView(const RadialSweepView& handle);
+
+  /**
+   * Assignment operator
+   */
+  RadialSweepView& operator=(const RadialSweepView& rhs);
+
+  /**
+   * Destructor
+   */
+  ~RadialSweepView();
+
+  /**
+   * Downcast method
+   */
+  static RadialSweepView DownCast( BaseHandle handle );
+
+public: // Not for use by application developers
+
+  RadialSweepView( RadialSweepViewImpl& impl );
+
+  RadialSweepView( Dali::Internal::CustomActor* impl );
+};
+
+#endif

dalihub/dali-demo

Change-Id: I6007af3cd1f01b9e8845b9edf8cbbffdf2eef6c6

examples/radial-menu/radial-sweep-view-impl.h
@@ -1,0 +1,118 @@
+#ifndef DALI_DEMO_RADIAL_SWEEP_VIEW_IMPL_H
+#define DALI_DEMO_RADIAL_SWEEP_VIEW_IMPL_H
+
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <dali-toolkit/dali-toolkit.h>
+#include "radial-sweep-view.h"
+
+
+/********************************************************************************
+ * Class to implement a layer with a radial sweep stencil mask and an actor tree
+ */
+class RadialSweepViewImpl : public Dali::Toolkit::Internal::Control
+{
+public:
+  static RadialSweepView New();
+
+  static RadialSweepView New( float duration,
+                              float diameter,
+                              Dali::Radian initialAngle,
+                              Dali::Radian finalAngle,
+                              Dali::Radian initialSector,
+                              Dali::Radian finalSector );
+
+  RadialSweepViewImpl( float duration,
+                       float diameter,
+                       Dali::Radian initialAngle,
+                       Dali::Radian finalAngle,
+                       Dali::Radian initialSector,
+                       Dali::Radian finalSector );
+
+  void SetDuration(float duration);
+  void SetEasingFunction( Dali::AlphaFunction easingFunction );
+
+  void SetDiameter(float diameter);
+  void SetInitialAngle( Dali::Radian initialAngle);
+  void SetFinalAngle( Dali::Radian finalAngle);
+  void SetInitialSector( Dali::Radian initialSector);
+  void SetFinalSector( Dali::Radian finalSector);
+  void SetInitialActorAngle( Dali::Radian initialAngle );
+  void SetFinalActorAngle( Dali::Radian finalAngle );
+
+  float GetDuration( );
+  float GetDiameter( );
+  Dali::Radian GetInitialAngle( );
+  Dali::Radian GetFinalAngle( );
+  Dali::Radian GetInitialSector( );
+  Dali::Radian GetFinalSector( );
+  Dali::Radian GetInitialActorAngle( );
+  Dali::Radian GetFinalActorAngle( );
+
+  void RotateActorsWithStencil(bool rotate);
+
+  void Add( Dali::Actor actor );
+
+  void Activate( Dali::Animation anim = Dali::Animation(), float offsetTime=0, float duration=2.0f );
+
+  void Deactivate();
+
+private:
+
+  /**
+   * Create the stencil mask
+   */
+  void CreateStencil(Dali::Radian initialSector );
+
+private:
+  Dali::Layer           mLayer;
+  Dali::Animation       mAnim;
+  float                 mDuration;
+  float                 mDiameter;
+  Dali::Radian          mInitialAngle;
+  Dali::Radian          mFinalAngle;
+  Dali::Radian          mInitialSector;
+  Dali::Radian          mFinalSector;
+  Dali::Radian          mInitialActorAngle;
+  Dali::Radian          mFinalActorAngle;
+  Dali::AlphaFunction   mEasingFunction;
+  Dali::Actor           mStencilActor;       ///< Stencil actor which generates mask
+  Dali::Property::Index mStartAngleIndex;    ///< Index of start-angle property
+  Dali::Property::Index mRotationAngleIndex; ///< Index of rotation-angle property
+  bool                  mRotateActorsWithStencil:1;
+  bool                  mRotateActors;
+};
+
+
+inline RadialSweepViewImpl& GetImpl( RadialSweepView& obj )
+{
+  DALI_ASSERT_ALWAYS(obj);
+  Dali::RefObject& handle = obj.GetImplementation();
+  return static_cast<RadialSweepViewImpl&>(handle);
+}
+
+inline const RadialSweepViewImpl& GetImpl( const RadialSweepView& obj )
+{
+  DALI_ASSERT_ALWAYS(obj);
+  const Dali::RefObject& handle = obj.GetImplementation();
+  return static_cast<const RadialSweepViewImpl&>(handle);
+}
+
+
+
+#endif // DALI_DEMO_RADIAL_SWEEP_VIEW_IMPL_H

examples/radial-menu/radial-sweep-view.cpp
@@ -1,0 +1,184 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "radial-sweep-view.h"
+#include "radial-sweep-view-impl.h"
+
+using namespace Dali;
+
+RadialSweepView::RadialSweepView()
+{
+}
+
+RadialSweepView::RadialSweepView(const RadialSweepView& handle)
+: Control(handle)
+{
+}
+
+RadialSweepView& RadialSweepView::operator=(const RadialSweepView& rhs)
+{
+  if( &rhs != this )
+  {
+    Control::operator=(rhs);
+  }
+  return *this;
+}
+
+RadialSweepView::~RadialSweepView()
+{
+}
+
+RadialSweepView RadialSweepView::DownCast( BaseHandle handle )
+{
+  return Control::DownCast<RadialSweepView, RadialSweepViewImpl>(handle);
+}
+
+RadialSweepView RadialSweepView::New( )
+{
+  return RadialSweepViewImpl::New();
+}
+
+RadialSweepView RadialSweepView::New( float duration,
+                                      float diameter,
+                                      Radian initialAngle,
+                                      Radian finalAngle,
+                                      Radian initialSector,
+                                      Radian finalSector )
+{
+  return RadialSweepViewImpl::New(duration, diameter, initialAngle, finalAngle, initialSector, finalSector );
+}
+
+RadialSweepView::RadialSweepView( RadialSweepViewImpl& impl )
+: Control( impl )
+{
+}
+
+RadialSweepView::RadialSweepView( Dali::Internal::CustomActor* impl )
+: Control( impl )
+{
+  VerifyCustomActorPointer<RadialSweepViewImpl>(impl);
+}
+
+void RadialSweepView::SetDuration(float duration)
+{
+  GetImpl(*this).SetDuration(duration);
+}
+
+void RadialSweepView::SetEasingFunction( Dali::AlphaFunction easingFunction )
+{
+  GetImpl(*this).SetEasingFunction( easingFunction );
+}
+
+void RadialSweepView::SetDiameter(float diameter)
+{
+  GetImpl(*this).SetDiameter(diameter);
+}
+
+void RadialSweepView::SetInitialAngle( Dali::Radian initialAngle)
+{
+  GetImpl(*this).SetInitialAngle(initialAngle);
+}
+
+void RadialSweepView::SetFinalAngle( Dali::Radian finalAngle)
+{
+  GetImpl(*this).SetFinalAngle(finalAngle);
+}
+
+void RadialSweepView::SetInitialSector( Dali::Radian initialSector)
+{
+  GetImpl(*this).SetInitialSector(initialSector);
+}
+
+void RadialSweepView::SetFinalSector( Dali::Radian finalSector)
+{
+  GetImpl(*this).SetFinalSector(finalSector);
+}
+
+void RadialSweepView::SetInitialActorAngle( Dali::Radian initialAngle )
+{
+  GetImpl(*this).SetInitialActorAngle(initialAngle);
+}
+
+void RadialSweepView::SetFinalActorAngle( Dali::Radian finalAngle )
+{
+  GetImpl(*this).SetFinalActorAngle(finalAngle);
+}
+
+float RadialSweepView::GetDuration( )
+{
+  return GetImpl(*this).GetDuration();
+}
+
+float RadialSweepView::GetDiameter( )
+{
+  return GetImpl(*this).GetDiameter();
+}
+
+Dali::Radian RadialSweepView::GetInitialAngle( )
+{
+  return GetImpl(*this).GetInitialAngle();
+}
+
+Dali::Radian RadialSweepView::GetFinalAngle( )
+{
+  return GetImpl(*this).GetFinalAngle();
+}
+
+Dali::Radian RadialSweepView::GetInitialSector( )
+{
+  return GetImpl(*this).GetInitialSector();
+}
+
+Dali::Radian RadialSweepView::GetFinalSector( )
+{
+  return GetImpl(*this).GetFinalSector();
+}
+
+Dali::Radian RadialSweepView::GetInitialActorAngle( )
+{
+  return GetImpl(*this).GetInitialActorAngle();
+}
+
+Dali::Radian RadialSweepView::GetFinalActorAngle( )
+{
+  return GetImpl(*this).GetFinalActorAngle();
+}
+
+void RadialSweepView::RotateActorsWithStencil(bool rotate)
+{
+  GetImpl(*this).RotateActorsWithStencil(rotate);
+}
+
+void RadialSweepView::Add(Actor actor)
+{
+  GetImpl(*this).Add(actor);
+}
+
+void RadialSweepView::Activate()
+{
+  GetImpl(*this).Activate();
+}
+
+void RadialSweepView::Activate( Dali::Animation anim, float offsetTime, float duration )
+{
+  GetImpl(*this).Activate(anim, offsetTime, duration);
+}
+
+void RadialSweepView::Deactivate()
+{
+  GetImpl(*this).Deactivate();
+}

examples/radial-menu/radial-sweep-view.h
@@ -1,0 +1,150 @@
+#ifndef DALI_DEMO_RADIAL_SWEEP_VIEW_H
+#define DALI_DEMO_RADIAL_SWEEP_VIEW_H
+
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <dali-toolkit/dali-toolkit.h>
+
+class RadialSweepViewImpl;
+
+
+/********************************************************************************
+ * Handle to RadialSweepView implementation
+ */
+class RadialSweepView : public Dali::Toolkit::Control
+{
+public:
+  /**
+   * Create a new RadialSweepView with default parameters (2 second animation,
+   * no rotation, sweeping out a full circle).
+   */
+  static RadialSweepView New( );
+
+  /**
+   * Create a new RadialSweepView.
+   * @param[in] duration The duration of the sweep animation
+   * @param[in] diameter The diameter of the stencil mask
+   * @param[in] initialAngle The initial angle of the anticlockwise line of the sweep sector
+   * @param[in] finalAngle The final angle of the anticlockwise line of the sweep sector
+   * @param[in] initialSector The angle of the starting sector
+   * @param[in] finalSector The angle of the sector at the end of the animation.
+   * Note, to cover the entire circle, use a value of 359.9999 degrees, not zero or 360 degrees.
+   *
+   *            initial sector
+   *             \   |  .
+   *              \  |    .
+   * initialAngle  \ |     .   final sector
+   *                \|      _|
+   *                 .________
+   */
+  static RadialSweepView New( float duration,
+                              float diameter,
+                              Dali::Radian initialAngle,
+                              Dali::Radian finalAngle,
+                              Dali::Radian initialSector,
+                              Dali::Radian finalSector );
+
+  void SetDuration(float duration);
+
+  void SetEasingFunction( Dali::AlphaFunction easingFunction );
+
+  void SetDiameter(float diameter);
+
+  void SetInitialAngle( Dali::Radian initialAngle);
+
+  void SetFinalAngle( Dali::Radian finalAngle);
+
+  void SetInitialSector( Dali::Radian initialSector);
+
+  void SetFinalSector( Dali::Radian finalSector);
+
+  void SetInitialActorAngle( Dali::Radian initialAngle );
+
+  void SetFinalActorAngle( Dali::Radian finalAngle );
+
+  float GetDuration( );
+
+  float GetDiameter( );
+
+  Dali::Radian GetInitialAngle( );
+
+  Dali::Radian GetFinalAngle( );
+
+  Dali::Radian GetInitialSector( );
+
+  Dali::Radian GetFinalSector( );
+
+  Dali::Radian GetInitialActorAngle( );
+
+  Dali::Radian GetFinalActorAngle( );
+
+  /**
+   * @param[in] rotate True if the actors should rotate with the stencil
+   */
+  void RotateActorsWithStencil(bool rotate);
+
+  /**
+   * Add actors to the view
+   */
+  void Add(Actor actor);
+
+  /**
+   * Activate the sweep animation
+   */
+  void Activate( );
+
+  void Activate( Dali::Animation anim, float offsetTime, float duration );
+
+  /**
+   * Deactivate the sweep animation
+   */
+  void Deactivate();
+
+  /**
+   * Default constructor. Create an uninitialized handle.
+   */
+  RadialSweepView();
+
+  /**
+   * Copy constructor
+   */
+  RadialSweepView(const RadialSweepView& handle);
+
+  /**
+   * Assignment operator
+   */
+  RadialSweepView& operator=(const RadialSweepView& rhs);
+
+  /**
+   * Destructor
+   */
+  ~RadialSweepView();
+
+  /**
+   * Downcast method
+   */
+  static RadialSweepView DownCast( BaseHandle handle );
+
+public: // Not for use by application developers
+
+  RadialSweepView( RadialSweepViewImpl& impl );
+
+  RadialSweepView( Dali::Internal::CustomActor* impl );
+};
+
+#endif