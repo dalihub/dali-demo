3ef20b3f1c1e919858860f25f26c8885d364aad0
Paul Wisbey <p.wisbey@samsung.com>
2016-09-05 10:44:53 +0100

    Port the sparkle effect demo
    (Based on original demo by Xiangyin Ma)
    
    Change-Id: I9ac5d9b9c4cb18a21d0a12fae0225af5527e405e

--- demo/dali-demo.cpp
+++ demo/dali-demo.cpp
@@ -81,6 +81,7 @@ int DALI_EXPORT_API main(int argc, char **argv)
   demo.AddExample(Example("mesh-visual.example", DALI_DEMO_STR_TITLE_MESH_VISUAL));
   demo.AddExample(Example("primitive-shapes.example", DALI_DEMO_STR_TITLE_PRIMITIVE_SHAPES));
   demo.AddExample(Example("styling.example", DALI_DEMO_STR_TITLE_STYLING));
+  demo.AddExample(Example("sparkle.example", DALI_DEMO_STR_TITLE_SPARKLE));
 
   demo.SortAlphabetically( true );
 
