d7b2e68667afc951281db48367f5dfaff0bd55e2
Adeel Kazmi <adeel.kazmi@samsung.com>
2016-12-01 15:21:11 +0000

    Remove DALi Toy as it no longer compiles & refs to ShaderEffect
    Change-Id: I650cb43c8a5ad685f62422f5b1977f8dec5e4033

--- emscripten-examples/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-INSTALL_PATH=$DESKTOP_PREFIX/share/emscripten/
-
-echo "Installing Emscripten examples to: $INSTALL_PATH"
-
-mkdir -p $INSTALL_PATH
-cp -v *.{js,html} $INSTALL_PATH/
-
