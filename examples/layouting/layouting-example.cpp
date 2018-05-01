ccaff8d1583128cbae360c99dd826c9704842d66
agnelo vaz <agnelo.vaz@samsung.com>
2018-05-01 18:01:29 +0100

    Extending Layout tester demo
    Pressing "Change layout" will show another test example.
    
    Pressing Change button toggles padding on/off for second ImageView
    First Image View has a margin applied.
    
    Issues
    TogglePadding button is not unparented from stage
    
    Change-Id: I728071af85540df0a8fd27b7e78349e4f2370d2b

--- examples/layouting/layouting-example.cpp
+++ examples/layouting/layouting-example.cpp
@@ -17,13 +17,11 @@
 
 #include <string>
 #include "shared/view.h"
+#include "linear-container.h"
+#include "linear-container-padded.h"
 #include <dali/dali.h>
 #include <dali-toolkit/dali-toolkit.h>
-#include <dali-toolkit/devel-api/visuals/image-visual-properties-devel.h>
-#include <dali-toolkit/devel-api/visual-factory/visual-factory.h>
 #include <dali-toolkit/devel-api/controls/control-devel.h>
-#include <dali-toolkit/devel-api/layouting/hbox-layout.h>
-#include <dali-toolkit/devel-api/layouting/vbox-layout.h>
 
 using namespace Dali;
 using namespace Dali::Toolkit;
@@ -34,28 +32,7 @@ namespace
 const char* BACKGROUND_IMAGE( DEMO_IMAGE_DIR "lake_front.jpg" );
 const char* TOOLBAR_IMAGE( DEMO_IMAGE_DIR "top-bar.png" );
 
-const char* LTR_IMAGE( DEMO_IMAGE_DIR "icon-play.png" );
-const char* LTR_SELECTED_IMAGE( DEMO_IMAGE_DIR "icon-play-selected.png" );
-
-const char* RTL_IMAGE( DEMO_IMAGE_DIR "icon-reverse.png" );
-const char* RTL_SELECTED_IMAGE( DEMO_IMAGE_DIR "icon-reverse-selected.png" );
-
-const char* ROTATE_CLOCKWISE_IMAGE( DEMO_IMAGE_DIR "icon-reset.png" );
-const char* ROTATE_CLOCKWISE_SELECTED_IMAGE( DEMO_IMAGE_DIR "icon-reset-selected.png" );
-
-//const char* ROTATE_ANTICLOCKWISE_IMAGE( DEMO_IMAGE_DIR "icon-rotate-anticlockwise.png" );
-//const char* ROTATE_ANTICLOCKWISE_SELECTED_IMAGE( DEMO_IMAGE_DIR "icon-rotate-anticlockwise-selected.png" );
-
-
 const char* APPLICATION_TITLE( "Layout tester" );
-
-const char* IMAGE_PATH[] = {
-  DEMO_IMAGE_DIR "application-icon-101.png",
-  DEMO_IMAGE_DIR "application-icon-102.png",
-  DEMO_IMAGE_DIR "application-icon-103.png",
-  DEMO_IMAGE_DIR "application-icon-104.png"
-};
-const unsigned int NUMBER_OF_RESOURCES = sizeof(IMAGE_PATH) / sizeof(char*);
 
 }  // namespace
 
@@ -65,7 +42,9 @@ class LayoutingExample: public ConnectionTracker
 
   LayoutingExample( Application& application )
   : mApplication( application ),
-    mDirection( false )
+    mLinearContainer(),
+    mLinearContainerPadded(),
+    mLayoutIndex( 0 )
   {
     // Connect to the Application's Init signal
     mApplication.InitSignal().Connect( this, &LayoutingExample::Create );
@@ -79,6 +58,7 @@ class LayoutingExample: public ConnectionTracker
   void Create( Application& application )
   {
     // The Init signal is received once (only) during the Application lifetime
+
     auto stage = Stage::GetCurrent();
 
     auto bg = ImageView::New( BACKGROUND_IMAGE );
@@ -96,60 +76,45 @@ class LayoutingExample: public ConnectionTracker
     title.SetParentOrigin( ParentOrigin::CENTER );
     title.SetAnchorPoint( AnchorPoint::CENTER );
     title.SetProperty( TextLabel::Property::TEXT_COLOR, Color::BLUE );
-    title.SetProperty( TextLabel::Property::HORIZONTAL_ALIGNMENT, HorizontalAlignment::CENTER );
+    title.SetProperty( TextLabel::Property::HORIZONTAL_ALIGNMENT, HorizontalAlignment::LEFT );
     toolbar.Add( title );
 
-    mDirectionButton = PushButton::New();
-    mDirectionButton.SetProperty( PushButton::Property::UNSELECTED_ICON, RTL_IMAGE );
-    mDirectionButton.SetProperty( PushButton::Property::SELECTED_ICON, RTL_SELECTED_IMAGE );
-    mDirectionButton.ClickedSignal().Connect( this, &LayoutingExample::OnDirectionClicked );
-    mDirectionButton.SetParentOrigin( Vector3(0.33f, 1.0f, 0.5f ) );
-    mDirectionButton.SetAnchorPoint( AnchorPoint::BOTTOM_CENTER );
-    mDirectionButton.SetSize(75, 75);
-    stage.Add( mDirectionButton );
+    mNextLayout = PushButton::New();
+    mNextLayout.SetProperty( Toolkit::Button::Property::LABEL, "change layout");
+    mNextLayout.ClickedSignal().Connect( this, &LayoutingExample::ChangeLayout );
+    mNextLayout.SetParentOrigin( ParentOrigin::TOP_RIGHT );
+    mNextLayout.SetAnchorPoint( AnchorPoint::TOP_RIGHT );
+    mNextLayout.SetSize(175, 50);
+    toolbar.Add( mNextLayout );
 
-    mRotateButton = PushButton::New();
-    mRotateButton.SetProperty( PushButton::Property::UNSELECTED_ICON, ROTATE_CLOCKWISE_IMAGE );
-    mRotateButton.SetProperty( PushButton::Property::SELECTED_ICON, ROTATE_CLOCKWISE_SELECTED_IMAGE );
-    mRotateButton.ClickedSignal().Connect( this, &LayoutingExample::OnRotateClicked );
-    mRotateButton.SetParentOrigin( Vector3(0.66f, 1.0f, 0.5f ));
-    mRotateButton.SetAnchorPoint( Vector3(0.5f, 1.0f, 0.5f));
-    mRotateButton.SetSize(75, 75);
-    stage.Add( mRotateButton );
+    mLinearContainer.Demo::LinearContainer::Create();
+  }
 
-    // Create a hbox layout
-    mLinearContainer = Control::New();
-    mIsHorizontal = false;
+  bool ChangeLayout( Button button )
+  {
+    mLayoutIndex++;
 
-    auto layout = VboxLayout::New();
-    layout.SetAnimateLayout(true);
-    DevelControl::SetLayout( mLinearContainer, layout );
-
-    mLinearContainer.SetParentOrigin( ParentOrigin::CENTER );
-    mLinearContainer.SetAnchorPoint( AnchorPoint::CENTER );
-    mLinearContainer.SetName( "linearContainer" );
-    stage.Add( mLinearContainer );
-    mLinearContainer.SetProperty( Toolkit::LayoutItem::ChildProperty::WIDTH_SPECIFICATION, ChildLayoutData::MATCH_PARENT );
-    mLinearContainer.SetProperty( Toolkit::LayoutItem::ChildProperty::HEIGHT_SPECIFICATION, ChildLayoutData::MATCH_PARENT );
-    mLinearContainer.SetProperty( Actor::Property::LAYOUT_DIRECTION, LayoutDirection::LEFT_TO_RIGHT );
-
-    for( unsigned int x = 0; x < NUMBER_OF_RESOURCES; ++x )
+    switch( mLayoutIndex )
     {
-      mImageViews[x] = Toolkit::ImageView::New();
-      Property::Map imagePropertyMap;
-      imagePropertyMap[ Toolkit::Visual::Property::TYPE ] = Toolkit::Visual::IMAGE;
-      imagePropertyMap[ Toolkit::ImageVisual::Property::URL ] = IMAGE_PATH[ x ];
-      imagePropertyMap[ ImageVisual::Property::DESIRED_WIDTH ] = 100.0f;
-      imagePropertyMap[ ImageVisual::Property::DESIRED_HEIGHT ] = 100.0f;
-
-      mImageViews[x].SetProperty(Toolkit::ImageView::Property::IMAGE , imagePropertyMap );
-      mImageViews[x].SetName("ImageView");
-      mImageViews[x].SetAnchorPoint( AnchorPoint::TOP_LEFT );
-      mImageViews[x].SetResizePolicy( ResizePolicy::FIXED, Dimension::ALL_DIMENSIONS );
-      mLinearContainer.Add( mImageViews[x] );
+      case 1 :
+      {
+        mLinearContainer.Remove();
+        mLinearContainerPadded.Create();
+        break;
+      }
+      case 2 :
+      {
+        mNextLayout.SetProperty( Toolkit::Button::Property::DISABLED, true );
+        mLinearContainerPadded.Remove();
+        break;
+      }
+      default :
+      {
+        break;
+      }
     }
 
-    Stage::GetCurrent().KeyEventSignal().Connect(this, &LayoutingExample::OnKeyEvent);
+    return true;
   }
 
 private:
@@ -167,50 +132,13 @@ private:
     }
   }
 
-  bool OnDirectionClicked( Button button )
-  {
-    if( mDirection )
-    {
-      mDirectionButton.SetProperty( PushButton::Property::UNSELECTED_ICON, LTR_IMAGE );
-      mDirectionButton.SetProperty( PushButton::Property::SELECTED_ICON, LTR_SELECTED_IMAGE );
-      mLinearContainer.SetProperty( Actor::Property::LAYOUT_DIRECTION, LayoutDirection::LEFT_TO_RIGHT );
-    }
-    else
-    {
-      mDirectionButton.SetProperty( PushButton::Property::UNSELECTED_ICON, RTL_IMAGE );
-      mDirectionButton.SetProperty( PushButton::Property::SELECTED_ICON, RTL_SELECTED_IMAGE );
-      mLinearContainer.SetProperty( Actor::Property::LAYOUT_DIRECTION, LayoutDirection::RIGHT_TO_LEFT );
-    }
-    mDirection = !mDirection;
-    return true;
-  }
-
-  bool OnRotateClicked( Button button )
-  {
-    mIsHorizontal = !mIsHorizontal;
-    if( mIsHorizontal )
-    {
-      auto hboxLayout = HboxLayout::New();
-      hboxLayout.SetAnimateLayout(true);
-      DevelControl::SetLayout( mLinearContainer, hboxLayout );
-    }
-    else
-    {
-      auto vboxLayout = VboxLayout::New();
-      vboxLayout.SetAnimateLayout(true);
-      DevelControl::SetLayout( mLinearContainer, vboxLayout );
-    }
-    return true;
-  }
 
 private:
   Application& mApplication;
-  Toolkit::ImageView mImageViews[ NUMBER_OF_RESOURCES ];
-  PushButton mDirectionButton;
-  PushButton mRotateButton;
-  Control mLinearContainer;
-  bool mDirection;
-  bool mIsHorizontal = true;
+  Demo::LinearContainer mLinearContainer;
+  Demo::LinearContainerPadded mLinearContainerPadded;
+  PushButton mNextLayout;
+  unsigned int mLayoutIndex;
 };
 
 int DALI_EXPORT_API main( int argc, char **argv )
@@ -219,4 +147,4 @@ int DALI_EXPORT_API main( int argc, char **argv )
   LayoutingExample test( application );
   application.MainLoop();
   return 0;
-}
+};
---
+++ examples/layouting/linear-container.h
@@ -0,0 +1,74 @@
+#ifndef DALI_DEMO_LINEAR_CONTAINER_H
+#define DALI_DEMO_LINEAR_CONTAINER_H
+
+/*
+ * Copyright (c) 2018 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <string>
+#include "linear-container.h"
+#include <dali/dali.h>
+#include <dali-toolkit/dali-toolkit.h>
+
+using namespace Dali;
+using namespace Dali::Toolkit;
+
+namespace
+{
+  const char* IMAGE_PATH[] = {
+  DEMO_IMAGE_DIR "application-icon-101.png",
+  DEMO_IMAGE_DIR "application-icon-102.png",
+  DEMO_IMAGE_DIR "application-icon-103.png",
+  DEMO_IMAGE_DIR "application-icon-104.png"
+};
+
+}
+
+namespace Demo
+{
+
+class LinearContainer: public ConnectionTracker
+{
+static const unsigned int NUMBER_OF_RESOURCES = sizeof(IMAGE_PATH) / sizeof(char*);
+
+public:
+
+  // Creates a Linear Layout Example and displays it.
+  void Create();
+
+  // Remove and destroy this layout
+  void Remove();
+
+private:
+
+  // Changes the direction of the items.
+  bool OnDirectionClicked( Button button );
+
+  // Alternates the linear layout from horizontal to vertical
+  bool OnRotateClicked( Button button );
+
+private:
+  Toolkit::ImageView mImageViews[ NUMBER_OF_RESOURCES ];
+  PushButton mDirectionButton;
+  PushButton mRotateButton;
+  Control mLinearContainer;
+  bool mDirection;
+  bool mIsHorizontal = true;
+}; // class LinearContainer
+
+} // namespace Demo
+
+#endif //DALI_DEMO_LINEAR_CONTAINER_H
