1933f017ee82ee582fa886ba37ddfa7206a9a43d
Anton Obzhirov <a.obzhirov@samsung.com>
2019-06-28 14:34:11 +0100

    Update demo dali build script with clean option.
    Change-Id: I2cf9bb788b5f8ef04ea74611aeb60287388d5ecc

--- android/native-demo/dali/build.sh
+++ android/native-demo/dali/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -z "$NDK" ]; then
-  . env.sh
+  . ./env.sh
 fi
 
 if [ ! -d "$ANDROID_SDK" ]; then
@@ -11,6 +11,11 @@ fi
 if [ ! -d "$ANDROID_NDK" ]; then
   echo "Please install Android NDK into "$ANDROID_NDK
   exit 1
+fi
+
+if [ "$1" = "clean" ]; then
+echo "Cleaning dali..."
+rm -rf ../../../../dali-env/android
 fi
 
 if [ ! -d ../../../../dali-env/android ]; then
