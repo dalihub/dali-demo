00f02c1c10c41fdb8727d0673f5ae60cc127b042
Adeel Kazmi <adeel.kazmi@samsung.com>
2019-09-23 16:12:30 +0100

    Change background colors of the launcher
    - Also removed ununsed consts from dali-table-view
    - Also removed unused js file from scripts directory
    
    Change-Id: Ibb0c0d087ad19943518ce223feb1652f2e7de4e3

--- shared/dali-table-view.cpp
+++ shared/dali-table-view.cpp
@@ -87,15 +87,9 @@ const char * const SHAPE_IMAGE_TABLE[] =
 const int NUMBER_OF_SHAPE_IMAGES( sizeof( SHAPE_IMAGE_TABLE ) / sizeof( SHAPE_IMAGE_TABLE[0] ) );
 
 const int NUM_BACKGROUND_IMAGES = 18;
-const float BACKGROUND_SWIPE_SCALE = 0.025f;
 const float BACKGROUND_SPREAD_SCALE = 1.5f;
-const float SCALE_MOD = 1000.0f * Math::PI * 2.0f;
-const float SCALE_SPEED = 10.0f;
-const float SCALE_SPEED_SIN = 0.1f;
 
 const unsigned int BACKGROUND_ANIMATION_DURATION = 15000; // 15 secs
-
-const Vector4 BACKGROUND_COLOR( 0.3569f, 0.5451f, 0.7294f, 1.0f );
 
 const float BUBBLE_MIN_Z = -1.0;
 const float BUBBLE_MAX_Z = 0.0f;
