51c96224110c0d3c84890c973e8b4151a2a7b150
Eunki, Hong <eunkiki.hong@samsung.com>
2022-10-05 23:03:00 +0900

    Minor coverity issue fixes
    Fix some minor coverity issues that might has meanful
    
    1. Initialize member value
    2. Remove useless std::move
    3. Remove const keyword for function return value.
    
    Change-Id: Ie1ecaa3394607ea724a2074b991cee6681122bef
    Signed-off-by: Eunki, Hong <eunkiki.hong@samsung.com>

--- examples/builder/examples.cpp
+++ examples/builder/examples.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2022 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -164,6 +164,8 @@ public:
   FileWatcher(void);
   ~FileWatcher(void);
   explicit FileWatcher(const std::string& fn)
+  : mLastTime(0),
+    mstringPath{}
   {
     SetFilename(fn);
   };
@@ -187,7 +189,8 @@ private:
 };
 
 FileWatcher::FileWatcher(void)
-: mLastTime(0)
+: mLastTime(0),
+  mstringPath{}
 {
 }
 
