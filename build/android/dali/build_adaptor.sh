c5baff1296e5e5bd6d7393166f901fcf39922bc5
Adeel Kazmi <adeel.kazmi@samsung.com>
2024-07-11 14:46:34 +0100

    (Android) Moved some defines into one place
    Change-Id: I1ef96d8c80fa39bb628438c852af530f781e47ac

--- build/android/dali/build_adaptor.sh
+++ build/android/dali/build_adaptor.sh
@@ -19,9 +19,24 @@ else
   export PKG_CONFIG_PATH=${PREFIX}/lib/${ANDROID_ABI}/pkgconfig
   export DALI_DATA_RO_DIR=assets
   export DALI_DATA_RW_DIR=/data/data/com.sec.dali_demo/files
-  ${ANDROID_SDK}/cmake/3.22.1/bin/cmake -S ${DALI_DIR}/dali-adaptor/build/tizen -DCMAKE_C_COMPILER=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/clang -DCMAKE_CXX_COMPILER=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/clang++ -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK}/build/cmake/android.toolchain.cmake -DANDROID_PLATFORM=${ANDROID_PLATFORM} -DANDROID_ABI=${ANDROID_ABI} -DANDROID_NDK=${ANDROID_NDK} -DANDROID_STL=c++_shared -DCMAKE_CXX_FLAGS='-fexceptions -frtti -w -Wall -std=c++17' -DINCLUDE_DIR=${PREFIX}/include -DLIB_DIR=${PREFIX}/lib/${ANDROID_ABI} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DENABLE_TRACE=${ENABLE_TRACE} -DENABLE_PROFILE=ANDROID
+  # There are other exports in the calling build.sh script
+
+  ${ANDROID_CMAKE_COMMAND} -S ${DALI_DIR}/dali-adaptor/build/tizen \
+    -DCMAKE_C_COMPILER=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/clang \
+    -DCMAKE_CXX_COMPILER=${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin/clang++ \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK}/build/cmake/android.toolchain.cmake \
+    -DANDROID_PLATFORM=${ANDROID_PLATFORM} \
+    -DANDROID_ABI=${ANDROID_ABI} \
+    -DANDROID_NDK=${ANDROID_NDK} \
+    -DANDROID_STL=c++_shared \
+    -DCMAKE_CXX_FLAGS='-fexceptions -frtti -w -Wall -std=c++17' \
+    -DINCLUDE_DIR=${PREFIX}/include \
+    -DLIB_DIR=${PREFIX}/lib/${ANDROID_ABI} \
+    -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} \
+    -DENABLE_TRACE=${ENABLE_TRACE} \
+    -DENABLE_PROFILE=ANDROID
 fi
 
-make -j8 || exit 1
-make install || exit 1
+make -j8 install || exit 1
 cd -
--- build/android/dali/env.sh
+++ build/android/dali/env.sh
@@ -1,17 +1,17 @@
 if [ ! -d "$ANDROID_SDK" ]; then
-# try default path
-if [ -d "$HOME/Android/Sdk" ]; then
-export ANDROID_SDK=$HOME/Android/Sdk
-fi
+  # try default path
+  if [ -d "$HOME/Android/Sdk" ]; then
+    export ANDROID_SDK=$HOME/Android/Sdk
+  fi
 fi
 
 if [ ! -d "$ANDROID_NDK" ]; then
-if [ -d $ANDROID_SDK ]; then
-NDK_DIR=$(find $ANDROID_SDK -maxdepth 2 -name ndk-build | sed 's/\/ndk-build//')
-if [ -d "$NDK_DIR" ]; then
-export ANDROID_NDK=$NDK_DIR
-fi
-fi
+  if [ -d $ANDROID_SDK ]; then
+    NDK_DIR=$(find $ANDROID_SDK -maxdepth 2 -name ndk-build | sed 's/\/ndk-build//')
+    if [ -d "$NDK_DIR" ]; then
+      export ANDROID_NDK=$NDK_DIR
+    fi
+  fi
 fi
 
 if [ ! -d "$DALI_DIR" ]; then
